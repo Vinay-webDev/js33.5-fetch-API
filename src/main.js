a11d0b2bd94d43f88df279519d99c514427547e2
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-03 23:54:10 +0530

    ===>> requestObj function

--- src/main.js
+++ src/main.js
@@ -49,13 +49,23 @@ requestJoke("Bruce", "Lee"); */
 // this API is harmful they got their security license expired 536 days ago so I won't use this api ("http://api.icndb.com");
 
 //limitTo=[nerdy] ==>> to get only nerdy joke ==>> to select nerdy category;
+/*
 const requestJoke = async (firstName, lastName) =>  {
     const response = await fetch(`https://icanhazdadjoke.com/jokes/random?firstName${firstName}&lastName${lastName}&limitTo=[nerdy]`);
     const jsonResponse = await response.json();
     console.log(jsonResponse);
 }
 
-requestJoke("Bruce", "Lee");
+requestJoke("Bruce", "Lee"); */
+
+const getDataFromForm = () => {
+    const requestObj = {
+        firstName: "Brue",
+        lastName: "Lee",
+        categories: ["nerdy"]
+    };
+    return requestObj;
+}
 
 
 
