8eeb0044f4817228735cc1b6e303faf0df4ecd2e
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-07-03 23:50:54 +0530

    to select category of jokes
    limitTo=[nerdy]

--- src/main.js
+++ src/main.js
@@ -37,16 +37,25 @@ const postDadJoke = async (jokeObj) => {
 postDadJoke(jokeObject); */
 
 // url 
-
+/*
 const requestJoke = async (firstName, lastName) =>  {
     const response = await fetch(`https://icanhazdadjoke.com/jokes/random?firstName${firstName}&lastName${lastName}`);
     const jsonResponse = await response.json();
     console.log(jsonResponse);
 }
 
+requestJoke("Bruce", "Lee"); */
+
+// this API is harmful they got their security license expired 536 days ago so I won't use this api ("http://api.icndb.com");
+
+//limitTo=[nerdy] ==>> to get only nerdy joke ==>> to select nerdy category;
+const requestJoke = async (firstName, lastName) =>  {
+    const response = await fetch(`https://icanhazdadjoke.com/jokes/random?firstName${firstName}&lastName${lastName}&limitTo=[nerdy]`);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+}
+
 requestJoke("Bruce", "Lee");
-
-
 
 
 
